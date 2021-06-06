6d47018670f1d370ed45f2dbd2616e1391f91f41
walter <wallvesbeauty@gmail.com>
2021-06-06 13:56:39 -0300

    updated gps app

--- GpsTracker/gen/com/example/gpstracker/R.java
+++ GpsTracker/gen/com/example/gpstracker/R.java
@@ -982,14 +982,15 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int ic_launcher=0x7f020057;
+        public static final int windicon=0x7f020058;
     }
     public static final class id {
-        public static final int BtnActualizar=0x7f05003d;
-        public static final int BtnDesactivar=0x7f05003c;
+        public static final int BtnActualizar=0x7f050042;
+        public static final int BtnDesactivar=0x7f050043;
         public static final int LblEstado=0x7f05003f;
-        public static final int LblPosLatitud=0x7f050040;
-        public static final int LblPosLongitud=0x7f050041;
-        public static final int LblPosPrecision=0x7f05003e;
+        public static final int LblPosLatitud=0x7f05003e;
+        public static final int LblPosLongitud=0x7f05003d;
+        public static final int LblPosPrecision=0x7f050040;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1001,7 +1002,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050042;
+        public static final int action_settings=0x7f050044;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1020,6 +1021,8 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int lblDatos=0x7f050041;
+        public static final int linearLayout1=0x7f05003c;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
@@ -1102,7 +1105,8 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int activity_main=0x7f030017;
-        public static final int support_simple_spinner_dropdown_item=0x7f030018;
+        public static final int backup_activity_main=0x7f030018;
+        public static final int support_simple_spinner_dropdown_item=0x7f030019;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
--- GpsTracker/src/com/example/gpstracker/MainActivity.java
+++ GpsTracker/src/com/example/gpstracker/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.gpstracker;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.util.Log;
 import android.content.Context;
@@ -13,38 +10,122 @@ import android.widget.TextView;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.widget.Button;
+
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+//import android.widget.Toast;
+import java.io.IOException;
+import java.io.File;
+import java.io.PrintStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends Activity {
 	private LocationManager locManager;
 	private LocationListener locListener;
-  
 	//private Button btnActualizar;
 	//private Button btnDesactivar;
 	private TextView lblLatitud; 
 	private TextView lblLongitud;
 	private TextView lblPrecision;
 	private TextView lblEstado;
-
+	private TextView lblDatosGrabados;
+	String tag = "GpsTracker";
+	int datos_grabados = 0;
+	private Calendar calendar; 
+	private SimpleDateFormat dateFormat; 
+	private String date; 
+	private static final String FILE_NAME = "gps.csv";
+	private String file = Environment.getExternalStorageDirectory().getPath() + "/gps/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-          
-        //btnActualizar = (Button)findViewById(R.id.BtnActualizar);
-        //btnDesactivar = (Button)findViewById(R.id.BtnDesactivar);
-        lblLatitud = (TextView)findViewById(R.id.LblPosLatitud);
         lblLongitud = (TextView)findViewById(R.id.LblPosLongitud);
-        lblPrecision = (TextView)findViewById(R.id.LblPosPrecision);
+        lblLatitud = (TextView)findViewById(R.id.LblPosLatitud);
         lblEstado = (TextView)findViewById(R.id.LblEstado);
+        lblPrecision = (TextView)findViewById(R.id.LblPosPrecision);
+        lblDatosGrabados =  (TextView)findViewById(R.id.lblDatos);
+        dateFormat = new SimpleDateFormat("MM-dd-yyyy-hhmmss");
+        calendar = Calendar.getInstance();
+        date = dateFormat.format(calendar.getTime());
+        
+        Log.d(tag, "Inthe onCreate() event:" + date);
+    	try {
+			CreateDirectory();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	Log.d(tag, "Inthe onCreate() Creando archivo:" + date);
+        try {
+            FileOutputStream fileinput = new FileOutputStream(file +  date +  FILE_NAME);
+            PrintStream printstream = new PrintStream(fileinput);
+            printstream.print("Latitude,Longitude,Altitude,Time\n");
+            fileinput.close();
+        } catch (Exception e) {
+        	e.printStackTrace();
+        }
+    }
+ 
+    public void CreateDirectory() throws IOException {
+        File directory = new File(Environment.getExternalStorageDirectory().getPath() + "/gps");
+        if (!directory.exists()) {
+        	Log.d(tag, "Creating the directory "+ Environment.getExternalStorageDirectory().getPath() + "/gps");
+       		directory.mkdir();
+        }
     }
 
+    public void save(String txtLatitud, String txtLongitud, String txtAltitude) {
+    	//Log.d(tag, "saving:" + txtLongitud + " - " + txtLatitud);
+    	//Log.d(tag, "saving to:"+ Environment.getExternalStorageDirectory().getPath());
+        dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z'");
+        calendar = Calendar.getInstance();
+        String tmpdate = dateFormat.format(calendar.getTime());
+        try {
+            OutputStreamWriter file_writer = new OutputStreamWriter(new FileOutputStream(file +  date +  FILE_NAME,true));
+            BufferedWriter buffered_writer = new BufferedWriter(file_writer);
+            buffered_writer.write(txtLatitud + "," + txtLongitud + "," + txtAltitude + "," + tmpdate + "\n");
+            buffered_writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        datos_grabados = datos_grabados + 1;
+        lblDatosGrabados.setText("Datos: " + datos_grabados);
+    }
+
+    public void onRestart() {
+    	super.onRestart();
+    	Log.d(tag, "Inthe onRestart() event");
+    }
+    public void onResume() {
+    	super.onResume();
+    	Log.d(tag, "Inthe onResume() event");
+    }
+    public void onPause() {
+    	super.onPause();
+        Log.d(tag, "Inthe onPause() event");
+    }
+    public void onStop() {
+    	super.onStop();
+        Log.d(tag, "Inthe onStop() event");
+    }
+    public void onDestroy() {
+    	super.onDestroy();
+        Log.d(tag, "Inthe onDestroy() event");
+    }
     public void onClick(View view) {
 		comenzarLocalizacion();
 	}
 
-    public void onClick2(View v) {
+    public void onClick_salir(View v) {
 	   	locManager.removeUpdates(locListener);
+	    finish();
+        System.exit(0);
 	}
 
     private void comenzarLocalizacion()
@@ -58,18 +139,21 @@ public class MainActivity extends Activity {
     	//Mostramos la última posición conocida
     	mostrarPosicion(loc);
     	//Nos registramos para recibir actualizaciones de la posición
-    	Log.i("", "ComenzarLocalizacion: ");
     	locListener = new LocationListener() {
 	    	public void onLocationChanged(Location location) {
+	    		Log.d(tag, "Inthe onLocationChanged() event");
 	    		mostrarPosicion(location);
 	    	}
 	    	public void onProviderDisabled(String provider){
+	    		Log.d(tag, "Inthe onProviderDisabled() event");
 	    		lblEstado.setText("Provider OFF");
 	    	}
 	    	public void onProviderEnabled(String provider){
+	    		Log.d(tag, "Inthe onProviderEnabled() event");
 	    		lblEstado.setText("Provider ON ");
 	    	}
 	    	public void onStatusChanged(String provider, int status, Bundle extras){
+	    		Log.d(tag, "Inthe onStatusChanged() event");
 	    		lblEstado.setText("Provider Status: " + status);
 	    	}
     	};
@@ -78,19 +162,21 @@ public class MainActivity extends Activity {
     
     
     private void mostrarPosicion(Location loc) {
-    	Log.i("", "mostrarPosicion: ");
     	if(loc != null)
     	{
-    		lblLatitud.setText("Latitud: " + String.valueOf(loc.getLatitude()));
-    		lblLongitud.setText("Longitud: " + String.valueOf(loc.getLongitude()));
+        	String str_latitud = String.valueOf(loc.getLatitude());
+        	String str_longitud = String.valueOf(loc.getLongitude());
+        	String str_altitude = String.valueOf(loc.getAltitude());
+    		lblLongitud.setText("Longitud: " + str_longitud);
+        	lblLatitud.setText("Latitud: " + str_latitud);
     		lblPrecision.setText("Precision: " + String.valueOf(loc.getAccuracy()));
-    		Log.i("", String.valueOf(loc.getLatitude() + " - " + String.valueOf(loc.getLongitude())));
-    	}
-    	else
-    	{
-    		lblLatitud.setText("Latitud: (sin_datos)");
+    		Log.i("mostrarPosicion:", String.valueOf(loc.getLatitude() + " - " + String.valueOf(loc.getLongitude())));
+    		save(str_latitud, str_longitud, str_altitude);
+    	} else {
     		lblLongitud.setText("Longitud: (sin_datos)");
+    		lblLatitud.setText("Latitud: (sin_datos!)");
     		lblPrecision.setText("Precision: (sin_datos)");
+    		save("null","null","null");
     	}
     }
 }
